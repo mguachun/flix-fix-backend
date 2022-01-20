4d7653d55ed8174b143ea555b2ec0247ad026f20
Melissa Guachun <1212malka@gmail.com>
2022-01-19 23:42:14 -0500

    updated schema with image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_011036) do
+ActiveRecord::Schema.define(version: 2022_01_20_042303) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_12_06_011036) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "genre"
+    t.string "image"
   end
 
 end
